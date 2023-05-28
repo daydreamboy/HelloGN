5f816c8b82b311a3ce51728403597fdf09334769
daydreamboy <wesley4chen@gmail.com>
2023-05-28 19:51:32 +0800

    UseStaticCPPLib-iOS: add code

--- UseStaticCPPLib-iOS/build_ios.sh
+++ UseStaticCPPLib-iOS/build_ios.sh
@@ -1,3 +1,4 @@
+libName="libhello_ios_static_cpp.a"
 
 if [[ $1 = "device" ]]; then
   gn gen ios_out/arm64 --args='is_debug=true target_os="ios" target_cpu="arm64" ios_enable_code_signing=false'
@@ -13,7 +14,8 @@ elif [[ $1 = "all" ]]; then
   ninja -C ios_out/x64
 
   mkdir -p ./ios_out/all/obj
-  lipo -create ./ios_out/arm64/obj/libhello_ios_static_cpp.a ./ios_out/x64/obj/libhello_ios_static_cpp.a -o ./ios_out/all/obj/libhello_ios_static_cpp.a
+  lipo -create ./ios_out/arm64/obj/${libName} ./ios_out/x64/obj/${libName} -o ./ios_out/all/obj/${libName}
+  echo "create static library ($1) successfully!"
 else
   echo "Must support a parameter"
 fi
