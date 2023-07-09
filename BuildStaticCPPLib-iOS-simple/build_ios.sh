31fb9fb483a8c6ca113ef102fa90b34654938c58
daydreamboy <wesley4chen@gmail.com>
2023-07-09 17:37:28 +0800

    BuildStaticCPPLib-iOS-simple: add code

--- BuildStaticCPPLib-iOS-simple/build_ios.sh
+++ BuildStaticCPPLib-iOS-simple/build_ios.sh
@@ -4,16 +4,16 @@ set -e
 libName="libhello_ios_static_cpp.a"
 
 if [[ $1 = "device" ]]; then
-  gn gen ios_out/arm64 --args='is_debug=true target_os="ios" target_cpu="arm64" ios_enable_code_signing=false'
+  gn gen ios_out/arm64 --args='target_os="ios" target_cpu="arm64"'
   ninja -C ios_out/arm64
 elif [[ $1 = "simulator" ]]; then
-  gn gen ios_out/x64 --args='is_debug=true target_os="ios" target_cpu="x64" ios_enable_code_signing=false'
+  gn gen ios_out/x64 --args='target_os="ios" target_cpu="x64"'
   ninja -C ios_out/x64
 elif [[ $1 = "all" ]]; then
-  gn gen ios_out/arm64 --args='is_debug=true target_os="ios" target_cpu="arm64" ios_enable_code_signing=false'
+  gn gen ios_out/arm64 --args='target_os="ios" target_cpu="arm64"'
   ninja -C ios_out/arm64
 
-  gn gen ios_out/x64 --args='is_debug=true target_os="ios" target_cpu="x64" ios_enable_code_signing=false'
+  gn gen ios_out/x64 --args='target_os="ios" target_cpu="x64"'
   ninja -C ios_out/x64
 
   mkdir -p ./ios_out/all/obj
